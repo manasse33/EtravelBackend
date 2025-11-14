b648c6b020a2d63ad2cc7f6b0e8482b2f6473b07
Manasse AFOULA <manasseafoula33@gmail.com>
2025-11-14 11:13:06 +0100

    ouikenac get

--- app/Models/PackagePrice.php
+++ app/Models/PackagePrice.php
@@ -4,4 +4,12 @@ use Illuminate\Database\Eloquent\Model;
 class PackagePrice extends Model {
     protected $fillable = ['priceable_type','priceable_id','country_id','departure_country_id','arrival_country_id','min_people','max_people','price','currency','programme'];
     public function priceable(){ return $this->morphTo(); }
+    public function departureCountry() {
+    return $this->belongsTo(Country::class, 'departure_country_id');
+}
+
+public function arrivalCountry() {
+    return $this->belongsTo(Country::class, 'arrival_country_id');
+}
+
 }
